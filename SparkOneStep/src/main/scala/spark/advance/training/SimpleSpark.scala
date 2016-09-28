6afd49aacbffc5f914be5ac9ef2713ff88b29022
Sumit <sumitagrawal4548@gmail.com>
2016-09-28 09:37:52 +0530

    Test

--- SparkOneStep/src/main/scala/spark/advance/training/SimpleSpark.scala
+++ SparkOneStep/src/main/scala/spark/advance/training/SimpleSpark.scala
@@ -17,9 +17,9 @@ object SparkWordCount {
 
     // count the occurrence of each word
     val wordCounts = tokenized.map((_, 1)).reduceByKey(_ + _)
-
+     
     // filter out words with fewer than threshold occurrences
-    val filtered = wordCounts.filter(_._2 >= threshold)
+    val    filtered = wordCounts.filter(_._2 >= threshold)
 
     // count characters
     val charCounts = filtered.flatMap(_._1.toCharArray).map((_, 1)).reduceByKey(_ + _)
