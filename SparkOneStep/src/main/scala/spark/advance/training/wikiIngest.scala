f24e72223169ceee24a521c19cae74e3db8cfc20
Sumit <sumitagrawal4548@gmail.com>
2016-09-30 08:02:11 +0530

    AddedFirstWorkingCode

--- SparkOneStep/src/main/scala/spark/advance/training/wikiIngest.scala
+++ SparkOneStep/src/main/scala/spark/advance/training/wikiIngest.scala
@@ -6,24 +6,30 @@ import org.apache.spark.SparkConf
 import org.apache.hadoop.mapred.JobConf
 import org.apache.hadoop.io.Text
 import org.apache.hadoop.mapred.FileInputFormat
-import org.apache.spark.SparkConf 
+import org.apache.spark.SparkConf
 
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.apache.hadoop.streaming.StreamXmlRecordReader
+import org.apache.hadoop.mapred.JobConf
+import org.apache.hadoop.io.Text
+import org.apache.hadoop.mapred.FileInputFormat
 
-
-object Evaluater {
-  def main(args: Array[String]){
-    val conf =   new SparkConf().setAppName("Language Evaluator")
+object Evaluator {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("Language Evaluator").setMaster("local")
     val sc = new SparkContext(conf)
-    val jobconf = new JobConf()
-    jobconf.set("stream.recordreader.class", "org.apache.hadoop.streaming.StreamXmlRecordReader")
-    jobconf.set("stream.recordreader.begin","<page>")
-    jobconf.set("stream.recordreader.end","</page>")
-    
-    FileInputFormat.addInputPaths(jobconf, "file:///Data/WikiPages_BigData.xml")
-    
-    val wikiDocuments = sc.hadoopRDD(jobconf,classOf[org.apache.hadoop.streaming.StreamInputFormat],
-    classOf[Text],classOf[Text]    
-    )
+    val jobConf = new JobConf()
+    jobConf.set("stream.recordreader.class",
+      "org.apache.hadoop.streaming.StreamXmlRecordReader")
+    jobConf.set("stream.recordreader.begin", "<page>")
+    jobConf.set("stream.recordreader.end", "</page>")
+    FileInputFormat.addInputPaths(jobConf, "file:///C:/Users/Sumit/git/SparkInAction/SparkOneStep/Data/WikiPages_BigData.xml")
+
+    val wikiDocuments = sc.hadoopRDD(jobConf,
+      classOf[org.apache.hadoop.streaming.StreamInputFormat],
+      classOf[Text], classOf[Text])
+
+    wikiDocuments.take(5)
   }
-  
 }
